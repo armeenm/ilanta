05c76b7a528ac7ff7fa6c8d7428f5ea9f079035e
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-03-17 10:53:04 -0500

    Clean up #include's

--- ilanta/io/gpiod.hpp
+++ ilanta/io/gpiod.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <spdlog/fmt/bundled/core.h>
-#include <spdlog/spdlog.h>
 
 #include <exception>
 #include <gpiod.hpp>
@@ -9,19 +8,17 @@
 
 #include "ilanta/util/strify.hpp"
 
-#define ILANTA_GPIO_OUTPUT_THROW(l, ...)                                     \
-  do {                                                                       \
-    auto success =                                                           \
-        ilanta::request_output(l, CONSUMER_NAME __VA_OPT__(, ) __VA_ARGS__); \
-    if (!success)                                                            \
-      throw std::runtime_error(fmt::format(                                  \
-          "Failed to request " ILANTA_STRIFY(l) " line; offset {}",          \
-          (l).offset()));                                                    \
+#define ILANTA_GPIO_OUTPUT_THROW(l, ...)                                                           \
+  do {                                                                                             \
+    auto success = ilanta::request_output(l, CONSUMER_NAME __VA_OPT__(, ) __VA_ARGS__);            \
+    if (!success)                                                                                  \
+      throw std::runtime_error(                                                                    \
+          fmt::format("Failed to request " ILANTA_STRIFY(l) " line; offset {}", (l).offset()));    \
   } while (0)
 
 namespace ilanta {
 
-auto request_output(gpiod::line const& l, std::string_view consumer,
-                    bool invert = false, int default_val = 0) -> bool;
+auto request_output(gpiod::line const& l, std::string_view consumer, bool invert = false,
+                    int default_val = 0) -> bool;
 
-}  // namespace ilanta
+} // namespace ilanta
