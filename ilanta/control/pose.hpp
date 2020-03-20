1a05cbf7db2f6bddc65d51dcdda5329b0337b293
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-03-20 07:06:25 -0500

    Make PoseTL methods nodiscard

--- ilanta/control/pose.hpp
+++ ilanta/control/pose.hpp
@@ -10,10 +10,11 @@ struct PoseTL {
   float y = 0.0f;
   float z = 0.0f;
 
-  auto operator+(PoseTL const& other) const noexcept -> PoseTL {
+  [[nodiscard]] auto operator+(PoseTL const& other) const noexcept -> PoseTL {
     return PoseTL{x + other.x, y + other.y, z + other.y};
   }
-  auto operator-(PoseTL const& other) const noexcept -> PoseTL {
+
+  [[nodiscard]] auto operator-(PoseTL const& other) const noexcept -> PoseTL {
     return PoseTL{x - other.x, y - other.y, z - other.z};
   }
 };
