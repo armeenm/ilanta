24e8b2f4e59873be5a5ba915351e334c9fc5a683
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-04-03 09:19:34 -0500

    Add non-constexpr PoseTL norm test

--- test/test.cpp
+++ test/test.cpp
@@ -11,8 +11,10 @@ using namespace Catch::literals;
 TEST_CASE("Mathematical operations can be performed on PoseTL objects", "[PoseTL]") {
   auto constexpr p1 = PoseTL{0.0f, 3.5f, 2.2f};
   auto constexpr p2 = PoseTL{1.1f, 5.6f, 10.1f};
+  auto p3 = p1;
 
   REQUIRE(p1 + p2 == PoseTL{0.0f + 1.1f, 3.5f + 5.6f, 2.2f + 10.1f});
   REQUIRE(p1 - PoseTL{1.0f, 2.0f, 3.0f} < p2);
   REQUIRE(p1.l2norm() == 4.13401_a);
+  REQUIRE(p3.l2norm() == 4.13401_a);
 }
